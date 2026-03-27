f7a6f7539727dce4a0773a89b628aeb47253161a
alvaro0-pixel <alvarodalmaso07@gmail.com>
2026-03-27 16:20:51 -0300

    pagina 70 do pdf, até o item 134

--- app/src/main/java/com/example/exercicio4/util/Extensions.kt
+++ app/src/main/java/com/example/exercicio4/util/Extensions.kt
@@ -3,6 +3,9 @@ package com.example.exercicio4.util
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
+import com.example.exercicio4.R
+import com.example.exercicio4.databinding.BottomSheetBinding
+import com.google.android.material.bottomsheet.BottomSheetDialog
 
 fun Fragment.initToolbar(toolbar: Toolbar) {
     (activity as AppCompatActivity).setSupportActionBar(toolbar)
@@ -10,5 +13,24 @@ fun Fragment.initToolbar(toolbar: Toolbar) {
     (activity as AppCompatActivity).supportActionBar?.setDisplayHomeAsUpEnabled(true)
     toolbar.setNavigationOnClickListener {
         activity?.onBackPressedDispatcher?.onBackPressed()
+    }
+}
+
+fun Fragment.showBottomSheet(
+    titleDialog: Int? = null,
+    titleButton: Int? = null,
+    message: Int,
+    onClick: () -> Unit = {}
+){
+    val bottomSheetDialog = BottomSheetDialog(requireContext())
+    val binding: BottomSheetBinding =
+        BottomSheetBinding.inflate(layoutInflater, null, false)
+
+    binding.textviewTitle.text = getText(titleDialog ?: R.string.text_title_warning)
+    binding.textviewMessage.text = getText(message)
+    binding.buttonOk.text = getText(titleButton ?: R.string.text_button_warning)
+    binding.buttonOk.setOnClickListener {
+        onClick()
+        bottomSheetDialog.dismiss()
     }
 }
